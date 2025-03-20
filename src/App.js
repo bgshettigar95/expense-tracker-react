0d191db0e982da702f4d9ef2d1cc1a0ae179e113
Balakrishna Shettigar <bkshettigar93@gmail.com>
2025-03-20 13:43:56 +0530

    Initial code setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Navigate, Route } from 'react-router';
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
+import Home from './pages/Dashboard/Home';
+import Incomes from "./pages/Dashboard/Incomes";
+import Expenses from "./pages/Dashboard/Expenses";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return <div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Root />} />
+        <Route path="/login" exact element={<Login />} />
+        <Route path="/signUp" exact element={<SignUp />} />
+        <Route path="/dashboard" exact element={<Home />} />
+        <Route path="/incomes" exact element={<Incomes />} />
+        <Route path="/expenses" exact element={<Expenses />} />
+      </Routes>
+    </BrowserRouter>
+  </div>
 }
 
-export default App;
+const Root = () => {
+  // check if token exists in localStorage
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  //Redirect to dashboard if authenticated, else to login
+  return isAuthenticated ? <Navigate to="/dashboard" /> : <Navigate to="/login" />
+}
+
+export default App
